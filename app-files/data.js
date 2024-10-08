8c8c13416d60f5be26de428e304340d4df9f16d6
TanyaArchicad <tanyadiz2014@gmail.com>
2024-10-08 15:28:27 +0500

    Upload

---
+++ app-files/data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-arenda-prihozhaya-1",
+      "name": "Arenda Prihozhaya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6530181428784587,
+        "pitch": -0.006464182414795516,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.485323355546547,
+          "pitch": -0.10746603912476083,
+          "rotation": 0,
+          "target": "2-arenda-prihozhaya-2"
+        },
+        {
+          "yaw": 1.981660263377413,
+          "pitch": -0.3377729563207126,
+          "rotation": 0,
+          "target": "1-arenda-vanna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-arenda-vanna",
+      "name": "Arenda Vanna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.461780817101042,
+        "pitch": 0.012461498273076188,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5926052957378154,
+          "pitch": -0.06095350402262767,
+          "rotation": 0,
+          "target": "0-arenda-prihozhaya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-arenda-prihozhaya-2",
+      "name": "Arenda Prihozhaya 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4223985612166334,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1061082027956246,
+          "pitch": 0.03826452372872602,
+          "rotation": 0,
+          "target": "3-arenda-gostinnya-1"
+        },
+        {
+          "yaw": 1.5996201596195583,
+          "pitch": 0.00208582803786328,
+          "rotation": 0,
+          "target": "4-arenda-gostinnya-2"
+        },
+        {
+          "yaw": 2.9380331064198675,
+          "pitch": -0.13758714112915094,
+          "rotation": 0,
+          "target": "5-arenda-lodziya"
+        },
+        {
+          "yaw": 3.084585942822571,
+          "pitch": -0.34968161349095084,
+          "rotation": 0,
+          "target": "8-arenda-kuhnya"
+        },
+        {
+          "yaw": -1.8244133493558277,
+          "pitch": -0.021602502078213348,
+          "rotation": 0,
+          "target": "1-arenda-vanna"
+        },
+        {
+          "yaw": -1.5979003707281123,
+          "pitch": -0.005953432832807692,
+          "rotation": 0,
+          "target": "0-arenda-prihozhaya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-arenda-gostinnya-1",
+      "name": "Arenda Gostinnya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3122204398228057,
+        "pitch": 0.10940454968694091,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4548012116886113,
+          "pitch": -0.06111941846692304,
+          "rotation": 0,
+          "target": "5-arenda-lodziya"
+        },
+        {
+          "yaw": -0.47498268632032037,
+          "pitch": -0.01710051423261838,
+          "rotation": 0,
+          "target": "6-arenda-spalnya-1"
+        },
+        {
+          "yaw": 0.863718916602144,
+          "pitch": -0.11539192506731766,
+          "rotation": 0,
+          "target": "8-arenda-kuhnya"
+        },
+        {
+          "yaw": 2.7040668877854372,
+          "pitch": -0.0813499392896091,
+          "rotation": 0,
+          "target": "0-arenda-prihozhaya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-arenda-gostinnya-2",
+      "name": "Arenda Gostinnya 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.020841004559954968,
+        "pitch": 0.02144036895738033,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16514090056508834,
+          "pitch": -0.01948409267905582,
+          "rotation": 0,
+          "target": "2-arenda-prihozhaya-2"
+        },
+        {
+          "yaw": 1.6858209358494545,
+          "pitch": -0.32041304972656803,
+          "rotation": 0,
+          "target": "6-arenda-spalnya-1"
+        },
+        {
+          "yaw": -2.8323154911411343,
+          "pitch": -0.1884824221497734,
+          "rotation": 0,
+          "target": "5-arenda-lodziya"
+        },
+        {
+          "yaw": -1.7986638303430063,
+          "pitch": 0.009238051313104734,
+          "rotation": 0,
+          "target": "8-arenda-kuhnya"
+        },
+        {
+          "yaw": 1.3923218504302248,
+          "pitch": 0.013337670430749071,
+          "rotation": 0,
+          "target": "7-arenda-spalnya-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-arenda-lodziya",
+      "name": "Arenda Lodziya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.640796784967204,
+        "pitch": 0.0301661846023773,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1026647837602566,
+          "pitch": -0.08264917453585952,
+          "rotation": 0,
+          "target": "8-arenda-kuhnya"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-arenda-spalnya-1",
+      "name": "Arenda Spalnya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1931475110571892,
+        "pitch": -0.0021698792358630925,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6070487826995947,
+          "pitch": 0.000477866951458239,
+          "rotation": 0,
+          "target": "7-arenda-spalnya-2"
+        },
+        {
+          "yaw": -1.6774944432413825,
+          "pitch": 0.01575894010056622,
+          "rotation": 0,
+          "target": "8-arenda-kuhnya"
+        },
+        {
+          "yaw": -1.1181930394760897,
+          "pitch": -0.2319581624299456,
+          "rotation": 0,
+          "target": "3-arenda-gostinnya-1"
+        },
+        {
+          "yaw": -0.6174345851749585,
+          "pitch": -0.03521186519184916,
+          "rotation": 0,
+          "target": "2-arenda-prihozhaya-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-arenda-spalnya-2",
+      "name": "Arenda Spalnya 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.026236142253552,
+        "pitch": 0.02797707848548825,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3026341969798594,
+          "pitch": 0.05475546498744066,
+          "rotation": 0,
+          "target": "6-arenda-spalnya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-arenda-kuhnya",
+      "name": "Arenda Kuhnya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0573127625398655,
+        "pitch": -0.05817764173313833,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.159316932873482,
+          "pitch": 0.030175709439696163,
+          "rotation": 0,
+          "target": "4-arenda-gostinnya-2"
+        },
+        {
+          "yaw": 1.6688542901433685,
+          "pitch": -0.21949698328624834,
+          "rotation": 0,
+          "target": "6-arenda-spalnya-1"
+        },
+        {
+          "yaw": 2.5903978813823336,
+          "pitch": -0.007043168185191462,
+          "rotation": 0,
+          "target": "5-arenda-lodziya"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
